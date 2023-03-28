3dbb2d298be03cccf0c58868b3f3890ea31ef520
*SonHaiDang* <haidangson09@gmail.com>
2023-03-28 23:36:06 +0700

    cai dat xu li loi

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const cors = require('cors');
 const app = express();
 const contactRouter = require('./app/routes/contac.route')
-
+const ApiError = require('./app/api-error');
 
 // port
 require('dotenv').config();
@@ -11,8 +11,19 @@ const port = process.env.PORT;
 app.use(cors());
 app.use(express.json());
 
+// call api
 app.use('/api/contacts',contactRouter)
 
+// error 
+app.use((req, res, next) => {   
+    return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((err, req, res, next) => {  
+    return res.status(err.statusCode || 500).json({
+    message: err.message || "Internal Server Error",
+    });
+});
 app.listen(port, () =>
     console.log(`App listening at http://localhost:${port}`),
 );
